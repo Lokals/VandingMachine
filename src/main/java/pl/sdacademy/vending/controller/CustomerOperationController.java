2bc04c3f3ddfceedebcf3f8f0407e9236b02e7ad
Marcin Kowalewski <kowalewski.marcinn@gmail.com>
2018-11-03 13:14:26 +0100

    Added tests configuration class

--- src/main/java/pl/sdacademy/vending/controller/CustomerOperationController.java
+++ src/main/java/pl/sdacademy/vending/controller/CustomerOperationController.java
@@ -14,30 +14,30 @@ public class CustomerOperationController {
     public void printMachine(){
         char letter = 'A';
 
-        for (int k = 0; k < machine.colsCount(); k++) {
-            for (int r = 0; r < machine.rowCount( ); r++) {
-                printUpperBoundary(r,k);
+        for (int r = 0; r < machine.rowCount( ); r++) {
+            for (int k = 0; k < machine.colsCount(); k++) {
+                printUpperBoundary(k,r);
             }
             System.out.println( );
-            for (int r = 1; r <= machine.rowCount( ); r++) {
-                printSymbol(r,k, letter);
+            for (int k = 1; k <= machine.colsCount( ); k++) {
+                printSymbol(k,r, letter);
 
             }
             System.out.println( );
-            for (int r = 0; r < machine.rowCount( ); r++) {
-                printLowerBoundary(r,k);
+            for (int k = 0; k < machine.colsCount( ); k++) {
+                printLowerBoundary(k,r);
             }
             letter++;
             System.out.println( );
         }
     }
-    private void  printUpperBoundary(int r, int k){
+    private void  printUpperBoundary(int k, int r){
         System.out.print("+--------+");
     }
-    private void  printSymbol(int r, int k, char letter){
-        System.out.print("|   " + letter + r + "   |");
+    private void  printSymbol(int k, int r, char letter){
+        System.out.print("|   " + letter + k + "   |");
     }
-    private void  printLowerBoundary(int r, int k){
+    private void  printLowerBoundary(int k, int r){
         System.out.print("+--------+");
     }
 }
---
+++ src/test/java/pl/sdacademy/vending/util/ConfigurationTest.java
@@ -0,0 +1,64 @@
+package pl.sdacademy.vending.util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConfigurationTest {
+
+    private Configuration testedConfig;
+
+    @Before
+    public void init(){
+        testedConfig = new Configuration();
+    }
+
+    @Test
+    public void shouldReturnDefaultStringValueWhenPropartyisUnknown() {
+        // Given
+        String unknownPropertyName = "qwertdfasdc";
+        String expectedDefault = "javaIsOk";
+        // When
+        String propertyValue = testedConfig.getStringProperty(unknownPropertyName, expectedDefault);
+
+        // Then
+        assertEquals(expectedDefault,propertyValue);
+
+    }
+
+    @Test
+    public void shouldReturnSetValueWhenPropertyIsKnown() {
+        // Given
+        String knownPropertyName = "test.property.string";
+        String expectedDefault = "javaIsOk";
+        // When
+        String properValue = testedConfig.getStringProperty(knownPropertyName, expectedDefault);
+        // Then
+        assertEquals(properValue,"qwerty");
+    }
+
+    @Test
+    public void shouldReturnDefaultLongValue() {
+        // Given
+        String unknownPropertyName = "qwertdfasdc";
+        Long expectedDefault = 8L;
+        // When
+        Long longProperty = testedConfig.getLongProperty(unknownPropertyName, expectedDefault);
+        // Then
+        assertEquals(longProperty,expectedDefault );
+    }
+    @Test
+    public void shouldReturnSetLongValue() {
+        // Given
+        String knownPropertyName = "test.property.long";
+        Long expectedDefault = 8L;
+        Long expectedVal = 5L;
+        // When
+        Long longProperty = testedConfig.getLongProperty(knownPropertyName, expectedDefault);
+        // Then
+        assertEquals(expectedVal, longProperty);
+    }
+
+
+}
