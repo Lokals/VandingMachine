512e956da92a43216c9112bc6f5ce2189b0ceb32
Marcin Kowalewski <kowalewski.marcinn@gmail.com>
2018-11-04 13:14:27 +0100

    Added tests for formatMoney in StringUtil.class

--- src/main/java/pl/sdacademy/vending/util/StringUtil.java
+++ src/main/java/pl/sdacademy/vending/util/StringUtil.java
@@ -24,9 +24,44 @@ public class StringUtil {
                 y = y.append(" ");
 
             }
-            return y.delete(1,1)+text+y.append(" ");
+            return y.append(" ")+text+y.delete(1,1);
         }
 
         return text;
+    }
+    public static String formatMoney(Long value){
+
+        String valueString;
+
+
+        afterComma(value);
+        beforeComma(value);
+
+        valueString = beforeComma(value) + "," + afterComma(value);
+      return valueString;
+    }
+
+    private static String beforeComma(Long value) {
+        String integrals = Long.toString(value/100);
+        StringBuilder formaterMoney = new StringBuilder( );
+        Integer characterTillLastSpace = 0;
+        for (int charIndex = integrals.length() -1; charIndex >= 0; charIndex--){
+                characterTillLastSpace++;
+            formaterMoney = formaterMoney.append(integrals.charAt(charIndex));
+            if (characterTillLastSpace >= 3) {
+                    formaterMoney.append(" ");
+                    characterTillLastSpace = 0;
+                }
+            }
+        return formaterMoney.reverse().toString().trim();
+    }
+
+    private static String afterComma(Long value) {
+        String decimals = Long.toString(value%100);
+
+        if(decimals.length() < 2){
+            decimals = "0" +decimals;
+        }
+        return decimals;
     }
 }
