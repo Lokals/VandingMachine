5f2c916e5ee59e267b89383829306f72082e2ae4
Marcin Kowalewski <kowalewski.marcinn@gmail.com>
2018-11-17 15:34:47 +0100

    Added saving for our store in vending machine

---
+++ src/main/java/pl/sdacademy/vending/service/DefaultEmployeeService.java
@@ -0,0 +1,52 @@
+package pl.sdacademy.vending.service;
+
+import pl.sdacademy.vending.controller.service.EmployeeService;
+import pl.sdacademy.vending.model.Tray;
+import pl.sdacademy.vending.model.VendingMachine;
+import pl.sdacademy.vending.service.repository.VendingMachineRepository;
+import pl.sdacademy.vending.util.Configuration;
+
+import java.util.Optional;
+
+public class DefaultEmployeeService implements EmployeeService {
+    private final VendingMachineRepository vendingMachineRepository;
+    private final Configuration configuration;
+
+    public DefaultEmployeeService(VendingMachineRepository vendingMachineRepository, Configuration configuration) {
+        this.vendingMachineRepository = vendingMachineRepository;
+
+        this.configuration = configuration;
+    }
+
+
+    @Override
+    public Optional<String> addTray(Tray tray) {
+        Optional<VendingMachine>  loadMachineRepository = vendingMachineRepository.load( );
+
+        VendingMachine vendingMachine = loadMachineRepository.orElseGet(()->new VendingMachine(configuration));
+
+        boolean b = vendingMachine.placeTray(tray);
+        if (b){
+            vendingMachineRepository.save(vendingMachine);
+            return Optional.empty();
+        }
+        else{
+            String sth = "Please recheck provided values";
+            return Optional.of(sth);
+        }
+
+
+        return Optional.empty( );
+    }
+}
+/*
+    1.Metoda  ta  powinna  pobrać  automat  z  repozytorium,  wprowadzić  na  nim  zmianę,
+    oraz  zapisać  automat  w  repozytorium
+    2.Jeżeli  udało  się  wprowadzić  zmianę,
+    to  metoda  zwraca  null(czyli  brak  komunikatu  błędu)
+    3.Jeżeli  nie  udało  się  wprowadzić  zmiany  w  automacie,  to  metoda  zwróci  ogólny  komunikat  błędu,
+      np.Couldnot  set  uptray,  pleasecheckprovidedvalues.
+
+
+
+   */
