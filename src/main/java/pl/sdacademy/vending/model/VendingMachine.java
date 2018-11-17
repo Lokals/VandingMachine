ded1a4eb94532486bbcb6d7de25b66407315f849
Marcin Kowalewski <kowalewski.marcinn@gmail.com>
2018-11-17 13:54:52 +0100

    Added posibility to add trey on position + tests

--- src/main/java/pl/sdacademy/vending/model/VendingMachine.java
+++ src/main/java/pl/sdacademy/vending/model/VendingMachine.java
@@ -25,32 +25,10 @@ public class VendingMachine {
         }
         trays = new Tray[rowsCount.intValue()][colsCount.intValue()];
 
-        Random random = new Random();
-        for (int i= 0; i < rowsCount; i++){
-            for (int j = 0; j < colsCount; j++){
-                if (random.nextInt(10) < 8) {
-                    generateTrayAtPosition(i, j);
-                }
-            }
-        }
+
     }
 
-    private void generateTrayAtPosition(int rowNo, int colNo) {
-        Random random = new Random();
-        long priceProduct = random.nextInt(901) + 100;
-        char symbolLetter = (char) ('A' + rowNo);
-        int symbolNumber = colNo + 1;
-        String symbol = "" + symbolLetter + symbolNumber;
-        int prodProbability = random.nextInt(10);
-        Tray.Builder trayBuilder = Tray.builder(symbol).price(priceProduct);
-        if (prodProbability == 1) {
-            trayBuilder = trayBuilder.product(Product.builder("Product" + symbol).build( ));
-        }
-        if (prodProbability == 0){
-            trayBuilder = trayBuilder.product(Product.builder("Product" + symbol).build( ));
-        }
-        trays[rowNo][colNo] = trayBuilder.build();
-    }
+
 
 
     public Optional<Tray> getTrayAtPosition(int r, int k){
@@ -94,10 +72,21 @@ public class VendingMachine {
 
     public boolean placeTray(Tray trayToBeAdded) {
         String symbol = trayToBeAdded.getSymbol();
+        if (symbol.length() !=2){
+            return false;
+        }
         int rowNum = symbol.charAt(0) -'A';
         int colNum = symbol.charAt(1) -'1';
-        trays[rowNum][colNum] = trayToBeAdded;
-        return true;
+        if (rowNum < 0 || rowNum >= rowsCount || colNum < 0 || colNum >=colsCount){
+            return false;
+        }
+        else if (trays[rowNum][colNum] == null) {
+            trays[rowNum][colNum] = trayToBeAdded;
+            return true;
+        }
+        else {
+            return false;
+        }
 
     }
 }
--- src/test/java/pl/sdacademy/vending/model/VendingMachineTest.java
+++ src/test/java/pl/sdacademy/vending/model/VendingMachineTest.java
@@ -61,7 +61,7 @@ public class VendingMachineTest {
         Configuration config = mock(Configuration.class);
 
         doReturn(6L).when(config).getLongProperty(eq("machine.size.rows"),anyLong());
-        doReturn(4L).when(config).getLongProperty(eq("machine.size.cols"),anyLong());
+        doReturn(0L).when(config).getLongProperty(eq("machine.size.cols"),anyLong());
         // When
         new VendingMachine(config);
         // Then
@@ -87,5 +87,61 @@ public class VendingMachineTest {
         assertEquals(tray, vendingMachineTested.getTrayAtPosition(0, 1).get());
 
     }
+    @Test
+    public void shouldNotBeAbleToAddTrayToTakenSlot()
+    {
+        // Given
+        Tray tray = Tray.builder("A2").build( );
+        Tray secondTray = Tray.builder("A2").build( );
+        Configuration config = mock(Configuration.class);
 
+        doReturn(6L).when(config).getLongProperty(eq("machine.size.rows"),anyLong());
+        doReturn(4L).when(config).getLongProperty(eq("machine.size.cols"),anyLong());
+
+        VendingMachine vendingMachineTested = new VendingMachine(config);
+        // When
+
+        boolean firstTrayPlacementResult = vendingMachineTested.placeTray(tray);
+        boolean secondTrayPlacementResult = vendingMachineTested.placeTray(secondTray);
+
+        // Then
+
+        assertFalse(secondTrayPlacementResult);
+        assertEquals(tray, vendingMachineTested.getTrayAtPosition(0, 1).get());
+
+    }
+    @Test
+    public void shouldNotBeAbleToAddTrayToNotExistingPosition()
+    {
+        // Given
+        Tray tray = Tray.builder("##").build( );
+        Configuration config = mock(Configuration.class);
+
+        doReturn(6L).when(config).getLongProperty(eq("machine.size.rows"),anyLong());
+        doReturn(4L).when(config).getLongProperty(eq("machine.size.cols"),anyLong());
+
+        VendingMachine vendingMachineTested = new VendingMachine(config);
+
+        // When
+        boolean placed = vendingMachineTested.placeTray(tray);
+        // Then
+        assertFalse(placed);
+    }
+    @Test
+    public void shouldNotBeAbleToAddTooLongSymbol()
+    {
+        // Given
+        Tray tray = Tray.builder("A12").build( );
+        Configuration config = mock(Configuration.class);
+
+        doReturn(6L).when(config).getLongProperty(eq("machine.size.rows"),anyLong());
+        doReturn(4L).when(config).getLongProperty(eq("machine.size.cols"),anyLong());
+
+        VendingMachine vendingMachineTested = new VendingMachine(config);
+
+        // When
+        boolean placed = vendingMachineTested.placeTray(tray);
+        // Then
+        assertFalse(placed);
+    }
 }
